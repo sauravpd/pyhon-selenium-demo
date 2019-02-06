e60492dae11f29bcdf9912b1698208e5066ecff0
saurav <sauravpd58@gmail.com>
2019-02-06 23:23:42 +0530

    commit changes in demo4.py

--- tests/demo4.py
+++ tests/demo4.py
@@ -1,9 +1,12 @@
 from selenium import webdriver
 
 driver=webdriver.Chrome()
+# open url
 driver.get('https://www.google.com/')
 try:
+    # find element by name
 	driver.find_element_by_name('q').send_keys('automation')
 	driver.quit()
 except Exception as e:
+    # print exception
 	print('Exception found')
