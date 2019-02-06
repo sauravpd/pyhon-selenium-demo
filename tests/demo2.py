5d077fc814c5b77ff07be17dc756055e561494a8
saurav <sauravpd58@gmail.com>
2019-02-06 23:26:25 +0530

    commit changes in demo2.py

--- tests/demo2.py
+++ tests/demo2.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 
 driver=webdriver.Ie()
 driver.get('http://www.google.com')
+#print the title
 print(driver.title)
+
+# sleep for 10 seconds
 time.sleep(10)
 driver.quit()
